0d73f2b296099c9161978b1dbd4d55c34c9167f2
ricardorda <ricardorda.ti@gmail.com>
2019-07-25 22:45:38 -0300

    Reading csv

--- suicide-rate/suiciderate.py
+++ suicide-rate/suiciderate.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+'''
+https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016
+'''
+
 import pandas as pd
+pd.set_option('display.max_columns', 500)
 
+DF = pd.read_csv('suiciderate.csv',
+                 header=0,
+                 names=['country', 'year', 'sex', 'age', 'suicides_no', 'population', 'suicides100k', 'country_year', 'HDI_for_year', 'gdp_for_year', 'gdp_per_capita', 'generation'])
 
+print(DF.columns)
+
+print('============================')
+print(DF.head())
