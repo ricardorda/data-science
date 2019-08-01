75f33070bbd6187f7729bb78d852fdbf5533cc02
ricardorda <ricardorda.ti@gmail.com>
2019-07-31 22:32:10 -0300

    update

--- suicide-rate/suiciderate.py
+++ suicide-rate/suiciderate.py
@@ -5,12 +5,22 @@ https://www.kaggle.com/russellyates88/suicide-rates-overview-1985-to-2016
 
 import pandas as pd
 pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+
 
 DF = pd.read_csv('suiciderate.csv',
                  header=0,
                  names=['country', 'year', 'sex', 'age', 'suicides_no', 'population', 'suicides100k', 'country_year', 'HDI_for_year', 'gdp_for_year', 'gdp_per_capita', 'generation'])
 
 print(DF.columns)
-
+print('============================')
+print(DF.info())
+print('============================')
+print(DF.describe())
 print('============================')
 print(DF.head())
+print('============================')
+print(DF['HDI_for_year'].value_counts(dropna=False))
+print('============================')
+#print(DF['HDI_for_year'].value_counts(dropna=True))
+print('============================')
