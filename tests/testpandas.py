27df05dd27963281fb73dbe17276465695104383
ricardorda <ricardorda.ti@gmail.com>
2019-08-23 22:01:22 -0300

    'update'

---
+++ tests/testpandas.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+testDict = {
+'name': [np.nan,'John','Peter','Charles'],
+'heights': [1.9, np.nan, 1.8, 1.7],
+'weight': [80,70,np.nan,90]}
+
+# Identifying null values
+df = pd.DataFrame(testDict)
+print(df)
+print(df.isnull())
+print(df.isnull().any(axis=0))
+print(df.isnull().any(axis=1))
+
+
