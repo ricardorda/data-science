1bef3f3891a5249fe7cc4f14d10f65471916b555
ricardorda <ricardorda.ti@gmail.com>
2019-08-23 22:32:56 -0300

    'update'

--- tests/testpandas.py
+++ tests/testpandas.py
@@ -4,13 +4,20 @@ import pandas as pd
 testDict = {
 'name': [np.nan,'John','Peter','Charles'],
 'heights': [1.9, np.nan, 1.8, 1.7],
-'weight': [80,70,np.nan,90]}
+'weight': [80,70,np.nan,90],
+'age': [15, 20, 25, 40]}
 
-# Identifying null values
 df = pd.DataFrame(testDict)
 print(df)
+
+# Identifying null values
 print(df.isnull())
-print(df.isnull().any(axis=0))
-print(df.isnull().any(axis=1))
 
+# Identifying the columns with null values
+print(df.loc[:, df.isnull().any(axis=0)])
 
+# Identifying the rows with null values
+print(df[df.isnull().any(axis=1)])
+
+# Identifying the columns/rows with null values
+print(df.loc[df.isnull().any(axis=1), df.isnull().any(axis=0)])
